7b2eda6ccb0b964040ca39812a5b75c51ad78754
sulik-gohst <myrzakanovsultan0@gmail.com>
2020-07-17 13:40:40 +0600

    as

---
+++ 9.py
@@ -0,0 +1,29 @@
+line1 = "A smart boy"
+print(line1[2])
+
+line2 = "I can't swim"
+print(line2[11])
+
+line3 = "I love my pat"
+print(line3[0: 5])
+
+line4 = "He's my brother"
+print(line4[0: 13])
+
+line5 = "It's your notebook"
+print(len(line5))
+print(line5[2::2])
+
+line6 = "on the higway"
+print(len(line6))
+print(line6[1::2])
+
+line7 = "Anna"
+print(line7[::-1])
+
+line8 = "Ich hasse ihn" #Deutsch
+print(len(line8))
+print(line8[-11::1])
+
+line9 = "Her brother name is Molodec" #English
+print(len(line9))
